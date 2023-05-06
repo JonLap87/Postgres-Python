b41adfef4c48ba8c128d234812ffd6abdfe705ec
Jon Lap <Lapchinevcer@mail.ru>
2023-05-06 12:05:40 +0300

    add optimishatoin files

--- postgresPython copy optimisation.py
+++ postgresPython copy optimisation.py
@@ -1,4 +1,5 @@
 import psycopg2
+from psycopg2.sql import SQL, Identifier
 
 
 def create_db(conn):
@@ -18,7 +19,7 @@ def create_db(conn):
 
         cur.execute("""
                 CREATE TABLE IF NOT EXISTS Phone(
-                number DECIMAL UNIQUE CHECK(number <= 99999999999),
+                number TEXT [],
                 client_id INTEGER REFERENCES Client(client_id));
                 """)
         conn.commit()
@@ -40,23 +41,30 @@ def add_phone (conn, client_id, number):
 			""", (client_id, number))
 	return cur.fetchall()
 
-def change_client(conn, client_id, name=None, surname=None, e_mail=None, number=None):
+
+def change_client(conn, client_id, name=None, surname=None, e_mail=None):
+    arg_list = {'name': name, "surname": surname, 'e_mail': e_mail}
+    for key, arg in arg_list.items():
+        if arg:
+            conn.execute(SQL("UPDATE Client SET {}=%s WHERE client_id=%s").format(Identifier(key)), (arg, client_id))
     conn.execute("""
-            UPDATE Client
-            SET name=%s, surname=%s, e_mail=%s
+            SELECT * FROM Client
             WHERE client_id=%s
-            RETURNING client_id, name, surname, e_mail;
-            """, (name, surname, e_mail,client_id))
+            """, client_id)
     return cur.fetchall()
 
+        
 def change_phone(conn, client_id, number):
-    conn.execute("""
-            UPDATE Phone
-            SET number=%s
+       arg_list = {'number': number, }
+       for key, arg in arg_list.items():
+        if arg:
+            conn.execute(SQL("UPDATE Phone SET {}=%s WHERE client_id=%s").format(Identifier(key)), (arg, client_id))
+       conn.execute("""
+            SELECT * FROM Phone
             WHERE client_id=%s
-            RETURNING client_id, number;
-            """, (number, client_id))
-    return cur.fetchall()
+            """, client_id)
+       return cur.fetchall()
+    
 
 def delete_phone(conn, client_id, number=None):
     conn.execute("""
@@ -79,27 +87,31 @@ def delete_client(conn, client_id, name=None, surname=None, e_mail=None, number=
     return cur.fetchall()
 
 
-def find_client(conn, name=None, surname=None, e_mail=None, number=None):
-    conn.execute("""
+def find_client(conn, client_id, name=None, surname=None, e_mail=None, number=None):
+      arg_list = {'name': name, "surname": surname, 'e_mail': e_mail, 'number': number}
+      for key, arg in arg_list.items():
+        if arg:
+            conn.execute(SQL("UPDATE Client SET {}=%s WHERE client_id=%s").format(Identifier(key)), (arg, client_id))
+      conn.execute("""
             SELECT c.name, c.surname, c.e_mail, p.number FROM Client AS c
             LEFT JOIN Phone AS p ON c.client_id = p.client_id
-            WHERE c.name=%s OR c.surname=%s OR c.e_mail=%s OR p.number=%s;
-            """, (name, surname, e_mail, number))
-    return cur.fetchall()
+            WHERE c.client_id=%s
+            """, client_id)
+      return cur.fetchall()
 
 conn = psycopg2.connect(database="netol_db", user="postgres", password="220261")
 
 create_db(conn)
 with conn.cursor() as cur:
-    print (add_client(cur, 'Tom', 'Tomas', 'adom@mail.ru'))
+    print (add_client(cur, 'Tom','Adoms', 'tomad@mail.ru'))
     print (add_client(cur, 'Jon', 'Varon', 'samual@mail.com'))
     print (add_client(cur, 'JonI', 'Varonas', 'joni@mail.com'))
-    print (add_phone(cur, '1', '89109101314'))
-    print (add_phone(cur, '2', '89109101012'))
-    print (add_phone(cur, '3', '89109103322'))
-    print (change_client(cur, '1','Tomas', 'Tom', 'adomsa@mail.ru' ))
-    print (change_phone(cur, '1', '89109101315'))
-    print (delete_phone(cur, '3'))
-    print (delete_client(cur, '3'))
-    print (find_client(cur, 'Jon'))
+    print (add_phone(cur, '1', ['89109101314', '89101111213']))
+    print (add_phone(cur, '2', ['89109101012']))
+    print (add_phone(cur, '3', ['89109103322', '89167231415']))
+    print(change_client(cur, '1', name='Tomas', e_mail='a_tomas@mail.ru'))
+    print (change_phone(cur, '1', ['89109101315']))
+    print (delete_phone(cur, '2'))
+    print (delete_client(cur, '2'))
+    print (find_client(cur,'3',))
     conn.commit()
